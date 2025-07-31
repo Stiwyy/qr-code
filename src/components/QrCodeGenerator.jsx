711a30c6723f7b2566dce014e7bacf9da48e8237
Stiwyy <andrinrenggli@outlook.com>
2025-07-31 20:13:12 +0200

    Fixed styling and structure not being added to git

--- src/components/QrCodeGenerator.jsx
+++ src/components/QrCodeGenerator.jsx
@@ -1,6 +1,18 @@
-function qrcode(){
-    return(
-        
+import React from "react";
+
+function QrCodeGenerator() {
+    return (
+        <div>
+            <form>
+                <label htmlFor="qr-input">Enter text or URL:</label>
+                <input id="qr-input" type="text" name="qrInput" />
+                <button type="button">Generate QR Code</button>
+            </form>
+            <div id="qr-code-display">
+                {/* QR code will be rendered here */}
+            </div>
+        </div>
     );
 }
-export default qrcode
+
+export default QrCodeGenerator;
