ed4538b592172ab1316ee06a04e42edd612e1735
Stiwyy <andrinrenggli@outlook.com>
2025-07-31 21:18:55 +0200

    add finder patterns

--- src/utils/qrCodeAlgorithm.js
+++ src/utils/qrCodeAlgorithm.js
@@ -1,3 +1,5 @@
+// Generation for QR-Code Version 3 only for now
+
 const FINDER_PATTERN = [
     [1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 1],
@@ -32,3 +34,13 @@ function createEmptyMatrix() {
     return new Array(29).fill().map(() => new Array(29).fill(null));
 }
 
+function addFinderPatterns(matrix) {
+    for (let y = 0; y < 7; y++) {
+        for (let x = 0; x < 7; x++) {
+            matrix[y][x] = FINDER_PATTERN[y][x];
+            matrix[y][matrix.length - 7 + x] = FINDER_PATTERN[y][x];
+            matrix[matrix.length - 7 + y][x] = FINDER_PATTERN[y][x];
+        }
+    }
+    return matrix;
+}
